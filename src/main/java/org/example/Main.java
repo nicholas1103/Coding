30572b35abcd6ac7ca3b3a6bf55de2c56a6bcb47
Tan Phong <0968710438p@gmail.com>
2024-03-27 09:59:24 +0700

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("BANG CUU CHUONG");
+        for(int i = 2; i < 10; i++){
+            System.out.println("Bang nhan " + i);
+            for (int j = 1; j <= 10; j++){
+                System.out.println(i + " x " + j + " = " + i*j);
+            }
+        }
     }
 }
