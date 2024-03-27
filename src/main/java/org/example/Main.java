428f4ab979c8ee34f17efacf7db37b23bc682fcd
Hoang Phuc <honagphuc1402@gmail.com>
2024-03-27 09:33:58 +0700

    hi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Main {
+    static String a = "hi";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!" + a);
     }
 }
